3a90ed500eb7a283a3e5ea75710ec4c99792057c
YJ Song <kalihman0515@gmail.com>
2020-09-04 01:02:54 +0900

    Implement throwing bomb and bomb effect

--- Assets/Scripts/BombAction.cs
+++ Assets/Scripts/BombAction.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class BombAction : MonoBehaviour
 {
+    public GameObject bombEffect;
     // 충돌했을때 처리
     private void OnCollisionEnter(Collision collision)
     {
+        // 이펙트 프리팹 생성
+        GameObject eff = Instantiate(bombEffect);
+
+        // 이펙트 프리팹의 위치는 수류탄 오브젝트 자신의 위치와 동일.
+        eff.transform.position = transform.position;
+        
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject firePosition;
+    public GameObject bombFactory;
+    public float throwPower = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
+
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        //     RaycastHit hitInfo = new RaycastHit();
+
+        //     if (Physics.Raycast(ray, out hitInfo))
+        //     {
+
+        //     }
+        }
+    }
+}
