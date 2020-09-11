d61dc368e5b46a2f374bb896ed9e41f704870a68
YJ Song <kalihman0515@gmail.com>
2020-09-11 21:06:59 +0900

    Add enemy asset and script with player recognition functionality

---
+++ Assets/Scripts/EnemyFSM.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFSM : MonoBehaviour
+{
+    // 에너미 상태 상수
+    enum EnemyState
+    {   
+        Idle,
+        Move,
+        Attack,
+        Return,
+        Damaged,
+        Die
+    }
+
+    EnemyState m_State;
+    
+    // 플레이어 발견 범위
+    public float findDistance = 8f;
+
+    // 플레이어 트랜스폼
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_State = EnemyState.Idle;
+        
+        player = GameObject.Find("Player").transform;
+
+        cc = GetComponent<CharacterControoler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(m_State)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.Damaged:
+                Damaged();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
+
+    void Idle()
+    {
+        if(Vector3.Distance(transform.position, player.position) < findDistance)
+        {
+            m_State = EnemyState.Move;
+            print("상태 전환: Idle -> Move");
+        }
+    }
+
+    void Move()
+    {
+
+    }
+
+    void Attack()
+    {
+
+    }
+    void Return()
+    {
+
+    }
+    void Damaged()
+    {
+
+    }
+    void Die()
+    {
+
+    }
+}
