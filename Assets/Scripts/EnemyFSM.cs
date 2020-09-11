5191f272eee6e00e7a5835d3c3c09a7406f6dc23
YJ Song <kalihman0515@gmail.com>
2020-09-11 23:44:43 +0900

    Implement move and attack function

--- Assets/Scripts/EnemyFSM.cs
+++ Assets/Scripts/EnemyFSM.cs
@@ -23,6 +23,21 @@ public class EnemyFSM : MonoBehaviour
     // 플레이어 트랜스폼
     Transform player;
 
+    // 공격 가능 범위
+    public float attackDistance = 2f;
+
+    // 이동 속도
+    public float moveSpeed =5f;
+
+    // 캐릭터 콘트롤러 컴포넌트
+    CharacterController cc;
+
+    // 누적 시간
+    float currentTime = 0;
+
+    // 공격 딜레이 시간
+    float attackDelay = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +45,7 @@ public class EnemyFSM : MonoBehaviour
         
         player = GameObject.Find("Player").transform;
 
-        cc = GetComponent<CharacterControoler>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -70,12 +85,38 @@ public class EnemyFSM : MonoBehaviour
 
     void Move()
     {
+        if(Vector3.Distance(transform.position, player.position) > attackDistance)
+        {
+            Vector3 dir = (player.position - transform.position).normalized;
 
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_State = EnemyState.Attack;
+            print("상태 전환: Move -> Attack");
+            currentTime = attackDelay;
+        }
     }
 
     void Attack()
     {
-
+        if (Vector3.Distance(transform.position, player.position) < attackDistance)
+        {
+            currentTime += Time.deltaTime;
+            if(currentTime > attackDelay)
+            {
+                currentTime = 0;
+                print("공격");
+            }
+        }
+        // 그렇지 않다면, 현재 상태를 이동으로 전환한다
+        else
+        {
+            m_State = EnemyState.Move;
+            print("상태 전환: Attack -> Move");
+            currentTime = 0;
+        }
     }
     void Return()
     {
