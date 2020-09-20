772ad5c28b97f3211f07ee963f9f4ff952ed6a90
YJ Song <kalihman0515@gmail.com>
2020-09-20 21:42:59 +0900

    Add user id and password input, save, and check function with ui

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    // 사용자 데이터를 새로 저장하거나 저장된 데이터를 읽어 사용자의 입력과 일치하는지 검사
+
+    // 사용자 아이디 변수
+    public InputField id;
+
+    // 사용자 패스워드 변수
+    public InputField password;
+
+    // 검사 텍스트 변수
+    public Text notify;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveUserData()
+    {
+
+        if (!CheckInput(id.text, password.text))
+        {
+            return;
+        }
+
+        if (!PlayerPrefs.HasKey(id.text))
+        {
+            PlayerPrefs.SetString(id.text, password.text);
+            notify.text = "아이디 생성이 완료됐습니다.";
+        }
+        else
+        {
+            notify.text = "이미 존재하는 아이디입니다.";
+        }
+    }
+
+    public void CheckUserData()
+    {
+        if (!CheckInput(id.text, password.text))
+        {
+            return;
+        }
+
+        string pass = PlayerPrefs.GetString(id.text);
+
+        if (password.text == pass)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            notify.text = "입력하신 아이디와 패스워드가 일치하지 않습니다";
+        }
+    }
+
+    bool CheckInput(string id, string pwd)
+    {
+        if (id == "" || pwd == "")
+        {
+            notify.text = "아이디 또는 패스워드를 입력해주세요.";
+            return false;
+        }
+        else
+        {   
+            return true;
+        }
+    }
+}
