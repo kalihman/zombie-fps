31ec0c2e835b5923f07d06d55d19c18db64b3375
YJ Song <kalihman0515@gmail.com>
2020-10-01 02:03:40 +0900

    Add weapon switch indication UI and sniper zoom UI effect

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -31,6 +31,20 @@ public class PlayerFire : MonoBehaviour
 
     public GameObject[] eff_Flash;
 
+    // 무기 아이콘 스프라이트 변수
+    public GameObject weapon01;
+    public GameObject weapon02;
+    
+    // 크로스헤어 스프라이트 변수
+    public GameObject crosshair01;
+    public GameObject crosshair02;
+    
+    // 마우스 오른쪽 버튼 클릭 아이콘 스프라이트 변수
+    public GameObject Weapon01_R;
+    public GameObject Weapon02_R;
+
+    public GameObject crosshair02_zoom;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -66,15 +80,25 @@ public class PlayerFire : MonoBehaviour
                     rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
                     break;
                 case WeaponMode.Sniper:
+                    // 줌 모드 상태가 아니라면 줌 모드로 변경 
                     if (!ZoomMode)
                     {
                         Camera.main.fieldOfView = 15f;
                         ZoomMode = true;
+                        
+                        // 줌 모드일 때 크로스헤어를 변경한다
+                        crosshair02_zoom.SetActive(true);
+                        crosshair02.SetActive(false);
                     }
+                    // 그렇지 않으면 원래 상태로 변경, 줌 모드 해제
                     else
                     {
                         Camera.main.fieldOfView = 60f;
                         ZoomMode = false;
+                        
+                        // 크로스헤어를 스나이퍼 모드로 돌려놓는다
+                        crosshair02_zoom.SetActive(false);
+                        crosshair02.SetActive(true);
                     }
                     break;
             }
@@ -111,17 +135,40 @@ public class PlayerFire : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             wMode = WeaponMode.Normal;
+            
+            // 스나이퍼 모드에서 일반 모드 키를 눌렀을 때 Weapon01_R_zoom은 비활성화, 줌 모드 해제
+            crosshair02_zoom.SetActive(false);
 
             // 카메라의 화면을 다시 원래대로 돌림
             Camera.main.fieldOfView = 60f;
 
             wModeText.text = "Normal Mode";
+            
+            // 1번 스프라이프는 활성화, 2번은 비활성화
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+            crosshair01.SetActive(true);
+            crosshair02.SetActive(false);
+            
+            // Weapon01_R는 활성화되고, Weapon02_R는 비활성화
+            Weapon01_R.SetActive(true);
+            Weapon02_R.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             wMode = WeaponMode.Sniper;
 
             wModeText.text = "Sniper Mode";
+            
+            // 1번 스프라이프는 비활성화, 2번은 활성화
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+            crosshair01.SetActive(false);
+            crosshair02.SetActive(true);
+            
+            // Weapon01_R는 비활성화되고, Weapon02_R는 활성화
+            Weapon01_R.SetActive(false);
+            Weapon02_R.SetActive(true);
         }
     }
 
